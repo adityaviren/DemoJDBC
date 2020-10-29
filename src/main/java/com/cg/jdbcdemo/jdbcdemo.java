53ac73e3d01960fe7166e88dbc511ac7f6982ddf
Aditya Viren <adityaviren@gmail.com>
2020-10-30 01:33:32 +0530

    ADDED JDBC and drivers

--- src/main/java/com/cg/jdbcdemo/jdbcdemo.java
+++ src/main/java/com/cg/jdbcdemo/jdbcdemo.java
@@ -1,6 +1,7 @@
 package com.cg.jdbcdemo;
 
 import java.sql.*;
+import java.util.Date;
 import java.util.Enumeration;
 
 public class jdbcdemo {
@@ -18,6 +19,8 @@ public class jdbcdemo {
 
         listDrivers();
 
+        //id  name  phone     address  departmentid  department  gender  basic_pay  deductions taxable_pay  income_tax  net_pay start
+
         try {
             System.out.println("Connecting to database:" + jdbcURL);
             con = DriverManager.getConnection(jdbcURL, userName, password);
@@ -25,7 +28,23 @@ public class jdbcdemo {
             String sql = "SELECT * from employee_payroll;";
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery(sql);
-            System.out.println(stmt.executeQuery(sql));
+            while(rs.next()){
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                int phone = rs.getInt("phone");
+                int deductions = rs.getInt("deductions");
+                int tax_pay = rs.getInt("taxable_pay");
+                int income_tax = rs.getInt("income_tax");
+                int net_pay = rs.getInt("net_pay");
+                String address = rs.getString("address");
+                String departmentid = rs.getString("departmentid");
+                String department = rs.getString("department");
+                Date start = rs.getDate("start");
+                char gender = rs.getString("gender").charAt(0);
+                double basic_pay = rs.getDouble("basic_pay");
+                System.out.println(id + "," + name + "," + phone + "," + address + "," + departmentid + "," +
+                        department + "," + basic_pay + "," + deductions + "," + net_pay + "," + start);
+            }
         }
         catch(Exception e) {
             e.printStackTrace();
